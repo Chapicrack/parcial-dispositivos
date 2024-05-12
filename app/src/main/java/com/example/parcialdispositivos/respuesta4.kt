880e23ecc3fc4674661ba8adebd74f7c6d2dcead
Chapicrack <ellapaa@ulasalle.edu.pe>
2024-05-12 05:25:32 -0500

    se termino 4 preguntas

--- app/src/main/java/com/example/parcialdispositivos/respuesta4.kt
+++ app/src/main/java/com/example/parcialdispositivos/respuesta4.kt
@@ -10,11 +10,12 @@ import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 
-class respuesta3 : Fragment(R.layout.fragment_respuesta3) {
+class respuesta4 : Fragment(R.layout.fragment_respuesta4) {
     var score=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var respuesta=""
@@ -22,22 +23,22 @@ class respuesta3 : Fragment(R.layout.fragment_respuesta3) {
             score=bundle?.getInt("score").toString().toInt()
             respuesta=bundle?.getString("correcta").toString()
         }
-        val recipiente=view.findViewById<TextView>(R.id.textView31)
+        val recipiente=view.findViewById<TextView>(R.id.textView49)
         if(respuesta=="okey"){
             recipiente.text="acertastes"
         }
         else{
             recipiente.text="fallastes"
         }
-        val botoncito=view.findViewById<Button>(R.id.button33)
+        val botoncito=view.findViewById<Button>(R.id.button46)
         botoncito.setOnClickListener{
             navegar(score,view)
         }
+
     }
     private fun navegar(score: Int, view: View) {
         val datos= bundleOf("score" to score)
-        view.findNavController().navigate(R.id.action_respuesta3_to_pregunta4,datos)
+        view.findNavController().navigate(R.id.action_respuesta4_to_pregunta5,datos)
 
     }
 }
-
